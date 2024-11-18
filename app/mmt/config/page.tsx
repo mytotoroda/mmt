045b4391887ff31f556f0e44e743aac12c9ee7f0
titi kim <mygs4id@gmail.com>
2024-11-18 18:31:36 +0700

    Initial commit

---
+++ app/mmt/config/page.tsx
@@ -0,0 +1,16 @@
+// app/mmt/positions/page.tsx
+'use client';
+
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function config() {
+  return (
+    <Box>
+      <Typography variant="h4" sx={{ mb: 3 }}>
+        포지션 관리
+      </Typography>
+      {/* 포지션 관리 컨텐츠 */}
+    </Box>
+  );
+}
